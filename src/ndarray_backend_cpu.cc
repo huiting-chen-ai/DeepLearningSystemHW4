eddae404f4d59b4181028f6475bfb8d536dd95dc
AvailableUsernameA <huc014@ucsd.edu>
2026-07-07 19:30:00 -0400

    1

--- src/ndarray_backend_cpu.cc
+++ src/ndarray_backend_cpu.cc
@@ -91,7 +91,19 @@ void EwiseSetitem(const AlignedArray& a, AlignedArray* out, std::vector<int32_t>
    *   offset: offset of the *out* array (not a, which has zero offset, being compact)
    */
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  int ndim = shape.size();
+  size_t total = 1;
+  for (int s : shape) total *= s;
+
+  for (size_t i = 0; i < total; ++i) {
+    size_t out_idx = offset;
+    size_t temp = i;
+    for (int d = ndim - 1; d >= 0; --d) {
+      out_idx += (temp % shape[d]) * strides[d];
+      temp /= shape[d];
+    }
+    out->ptr[out_idx] = a.ptr[i];
+  }
   /// END SOLUTION
 }
 
@@ -112,7 +124,16 @@ void ScalarSetitem(const size_t size, scalar_t val, AlignedArray* out, std::vect
    */
 
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  int ndim = shape.size();
+  for (size_t i = 0; i < size; ++i) {
+    size_t out_idx = offset;
+    size_t temp = i;
+    for (int d = ndim - 1; d >= 0; --d) {
+      out_idx += (temp % shape[d]) * strides[d];
+      temp /= shape[d];
+    }
+    out->ptr[out_idx] = val;
+  }
   /// END SOLUTION
 }
 
@@ -206,7 +227,14 @@ void Matmul(const AlignedArray& a, const AlignedArray& b, AlignedArray* out, uin
    */
 
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < p; j++) {
+      out->ptr[i*p+j] = 0;
+      for (size_t k = 0; k < n; k++) {
+        out->ptr[i*p+j] += a.ptr[i*n+k]*b.ptr[k*p+j];
+      }
+    }
+  }
   /// END SOLUTION
 }
 
@@ -236,7 +264,13 @@ inline void AlignedDot(const float* __restrict__ a,
   out = (float*)__builtin_assume_aligned(out, TILE * ELEM_SIZE);
 
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  for (size_t i = 0; i < TILE; i++) {
+    for (size_t j = 0; j < TILE; j++) {
+      for (size_t k = 0; k < TILE; k++) {
+        out[i*TILE+j] += a[i*TILE+k]*b[k*TILE+j];
+      }
+    }
+  }
   /// END SOLUTION
 }
 
@@ -262,7 +296,20 @@ void MatmulTiled(const AlignedArray& a, const AlignedArray& b, AlignedArray* out
    *
    */
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  size_t TILE_SIZE = TILE * TILE;
+  for (size_t idx = 0; idx < out->size; idx++) {
+    out->ptr[idx] = 0;
+  }
+  for (size_t i = 0; i < m/TILE; i++) {
+    for (size_t j = 0; j < p/TILE; j++) {
+      size_t out_offset = (i*(p/TILE)+j)*TILE_SIZE;
+      for (size_t k = 0; k < n/TILE; k++) {
+        size_t a_offset = (i*(n/TILE)+k)*TILE_SIZE;
+        size_t b_offset = (k*(p/TILE)+j)*TILE_SIZE;
+        AlignedDot(a.ptr+a_offset, b.ptr+b_offset, out->ptr+out_offset);
+      }
+    }
+  }
   /// END SOLUTION
 }
 
@@ -277,7 +324,12 @@ void ReduceMax(const AlignedArray& a, AlignedArray* out, size_t reduce_size) {
    */
 
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  for (size_t i = 0; i < out->size; i++) {
+    out->ptr[i] = a.ptr[i*reduce_size];
+    for (size_t j = i*reduce_size+1; j < (i+1)*reduce_size; j++) {
+      out->ptr[i] = std::max(a.ptr[j], out->ptr[i]);
+    }
+  }
   /// END SOLUTION
 }
 
@@ -292,7 +344,12 @@ void ReduceSum(const AlignedArray& a, AlignedArray* out, size_t reduce_size) {
    */
 
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  for (size_t i = 0; i < out->size; i++) {
+    out->ptr[i] = a.ptr[i*reduce_size];
+    for (size_t j = i*reduce_size+1; j < (i+1)*reduce_size; j++) {
+      out->ptr[i] = a.ptr[j] + out->ptr[i];
+    }
+  }
   /// END SOLUTION
 }
 

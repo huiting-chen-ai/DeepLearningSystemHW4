8c7f8dc967879444406d2845b7abb3d370fbc8b2
AvailableUsernameA <huc014@ucsd.edu>
2026-07-09 11:19:23 -0400

    1:logSumExp

--- python/needle/ops/ops_logarithmic.py
+++ python/needle/ops/ops_logarithmic.py
@@ -38,7 +38,7 @@ class LogSumExp(TensorOp):
         ### BEGIN YOUR SOLUTION
         # M = array_api.max(Z, axis=self.axes, keepdims=True)
         M = Z.max(axis=self.axes, keepdims=True).reshape(Z.shape)
-        r = array_api.log(array_api.sum(array_api.exp(Z-M), axis=self.axes, keepdims=True))+M
+        r = array_api.log(array_api.sum(array_api.exp(Z-M), axis=self.axes, keepdims=True)).reshape(Z.shape)+M
         return array_api.squeeze(r, axis=self.axes)
         ### END YOUR SOLUTION
 

fbd6865916448c950e4cc51ad84e3d7f91ff9a37
AvailableUsernameA <huc014@ucsd.edu>
2026-07-11 17:32:26 -0400

    kaiming

--- python/needle/init/init_initializers.py
+++ python/needle/init/init_initializers.py
@@ -5,30 +5,40 @@ from typing import Any
 
 def xavier_uniform(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    alpha = gain * math.sqrt(6/(fan_in+fan_out))
+    return rand(fan_in, fan_out, low=-alpha, high=alpha, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = gain * math.sqrt(2/(fan_in+fan_out))
+    return randn(fan_in, fan_out, std=std, **kwargs)
     ### END YOUR SOLUTION
 
 def kaiming_uniform(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2)
+    bound = gain * math.sqrt(3/fan_in)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 
 def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2)
+    bound = gain * math.sqrt(3/fan_in)
+    if shape is None:
+        return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
+    return rand(shape, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 def kaiming_normal(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2)
+    std = gain / math.sqrt(fan_in)
+    return randn(fan_in, fan_out, std=std, **kwargs)
     ### END YOUR SOLUTION

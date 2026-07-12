e7fde2a58945bf3f671b704024055193c038457d
AvailableUsernameA <huc014@ucsd.edu>
2026-07-12 10:56:45 -0400

    ResNet

--- python/needle/ops/ops_mathematic.py
+++ python/needle/ops/ops_mathematic.py
@@ -181,7 +181,7 @@ class Reshape(TensorOp):
 
     def compute(self, a):
         ### BEGIN YOUR SOLUTION
-        return array_api.reshape(a, self.shape)
+        return array_api.reshape(a.compact(), self.shape)
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad, node):
